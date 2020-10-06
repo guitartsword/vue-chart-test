b0ab25aca0b56e5878824c6ec29762311449f292
Isaias Valle <guitartsword@gmail.com>
2020-10-06 10:47:54 -0600

    using actions to handle events

--- src/components/GSTC.vue
+++ src/components/GSTC.vue
@@ -1,11 +1,11 @@
 <template>
-<div>
-  <div class="toolbox">
-    <button @click="updateFirstRow">Update first row</button>
-    <button @click="changeZoomLevel">Change zoom level</button>
+  <div>
+    <div class="toolbox">
+      <button @click="updateFirstRow">Update first row</button>
+      <button @click="changeZoomLevel">Change zoom level</button>
+    </div>
+    <div class="gstc-wrapper" ref="gstc"></div>
   </div>
-  <div class="gstc-wrapper" ref="gstc"></div>
-</div>
 </template>
 
 <script>
@@ -19,6 +19,24 @@ import 'gantt-schedule-timeline-calendar/dist/style.css';
 let gstc, state;
 
 // helper functions
+
+class ItemsClickActions {
+  constructor(element, data){
+    this.data = data;
+    this.onClick = this.onClick.bind(this);
+    element.addEventListener('click', this.onClick);
+  }
+  update(element, data){
+    this.data = data;
+  }
+  destroy(element){
+    element.removeEventListener('click', this.onClick);
+  }
+  onClick(event){
+    console.log(`Item ${this.data} clicked!`);
+    console.log(`Item ${event} clicked!`);
+  }
+}
 
 function generateRows(){
   const rows = {};
@@ -64,6 +82,11 @@ export default {
     state = GSTC.api.stateFromConfig({
       licenseKey:'====BEGIN LICENSE KEY====\nXOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJc\npY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw==\n====END LICENSE KEY====',
       plugins:[TimelinePointer(), Selection(), ItemResizing(), ItemMovement()],
+      actions: {
+        "chart-timeline-items": [
+          ItemsClickActions
+        ]
+      },
       list:{
         columns:{
           data:{
@@ -126,7 +149,7 @@ export default {
   margin: 0;
   padding: 0;
 }
-.toolbox{
+.toolbox {
   padding: 10px;
 }
 </style>
